0aff3cb232af4f1d12ac141c42139f74f3a5abf2
Funtimes909 <contact@funtimes909.xyz>
2024-10-11 23:56:02 +1300

    add screenshot script to edit screenshots instantly after taking them
    Signed-off-by: Funtimes909 <contact@funtimes909.xyz>

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -4,7 +4,13 @@ WINDOW=$(hyprctl activewindow | grep class | awk -F': ' '{print $2}')
 LOCATION="$HOME/Pictures/Screenshots/$(whoami)-$(date +"%Y-%m/%d")/${WINDOW}-$(date +"%I-%M-%S").png"
 
 region_capture() {
-	hyprshot -m region -z -o $HOME/Pictures/Screenshots/$(whoami)-$(date +"%Y-%m/%d/") -f $(date +"%I-%M-%S").png
+	TIME="$(date +"%I-%M-%S").png"
+	hyprshot -m region -z -o $HOME/Pictures/Screenshots/$(whoami)-$(date +"%Y-%m/%d/") -f $TIME
+}
+
+region_capture_edit() {
+        TIME="$(date +"%I-%M-%S").png"
+        hyprshot -m region -z -o $HOME/Pictures/Screenshots/$(whoami)-$(date +"%Y-%m/%d/") -f $TIME; krita $HOME/Pictures/Screenshots/$(whoami)-$(date +"%Y-%m/%d/")$TIME
 }
 
 active_capture() {
@@ -21,6 +27,8 @@ output_capture() {
 
 if [[ "$1" == "--region" ]]; then
 	region_capture
+elif [[ "$1" == "--region-edit" ]]; then
+        region_capture_edit
 elif [[ "$1" == "--active" ]]; then
 	active_capture
 elif [[ "$1" == "--full" ]]; then
